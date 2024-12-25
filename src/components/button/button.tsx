37c6e6409fbe55cacc7be6def4402ee0de47d3b0
Yeonu-Kim <ywk0524@snu.ac.kr>
2024-12-26 02:06:35 +0900

    💄 상세 페이지 스타일링

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -4,13 +4,13 @@ import { cva } from 'class-variance-authority';
 import { cn } from '../../utils/designSystem';
 
 const buttonVariants = cva(
-  'w-full py-2 transition-colors duration-200 rounded-md',
+  'w-full py-1 transition-colors duration-200 rounded-md',
   {
     variants: {
       variant: {
         default: 'text-white bg-orange hover:bg-orange-hover cursor-pointer',
         mint: 'text-white bg-mint hover:bg-mint-hover cursor-pointer',
-        white: 'text-black bg-white hover:text-orange cursor-pointer',
+        white: 'text-black bg-white hover:text-blue cursor-pointer',
         disable: 'bg-gray-100 text-gray-500 cursor-not-allowed',
       },
     },
--- src/pages/SurveyDetailPage.tsx
+++ src/pages/SurveyDetailPage.tsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 
 import { NavigationHeader } from '../components/header';
+import { Layout, MainLayout } from '../components/layout';
 import { useRouteNavigation } from '../hooks/useRouteNavigation';
 import { mockSurveys } from '../mocks/surveys';
 
@@ -12,33 +13,60 @@ export const SurveyDetailPage = () => {
 
   if (mockSurvey === undefined) {
     return (
-      <div>
-        <NavigationHeader title="설문조사 상세 페이지" to={toSurveyList} />
-        <p>존재하지 않는 설문조사입니다.</p>
-      </div>
+      <Layout>
+        <NavigationHeader title="🔍 설문조사 상세 페이지" to={toSurveyList} />
+        <MainLayout>
+          <p>존재하지 않는 설문조사입니다.</p>
+        </MainLayout>
+      </Layout>
     );
   }
 
   return (
-    <div>
-      <NavigationHeader title="설문조사 상세 페이지" to={toSurveyList} />
-      <p>{mockSurvey.title}</p>
-      <p>{mockSurvey.writer}</p>
-      <p>
-        {mockSurvey.contents.split('\n').map((line, index) => (
-          <span key={index}>
-            {line}
-            <br />
-          </span>
-        ))}
-      </p>
-      <a
-        href="https://forms.gle/64UkrJpRHHLdJQwi9"
-        target="_blank"
-        rel="noreferrer"
-      >
-        설문조사 이동하기
-      </a>
-    </div>
+    <Layout>
+      <NavigationHeader title="🔍 설문조사 상세 페이지" to={toSurveyList} />
+      <MainLayout>
+        <div className="flex flex-col py-2 gap-3 border-b">
+          <div className="flex gap-2 items-center">
+            {mockSurvey.winners.length === 0 ? (
+              <span className="text-xs p-1 rounded bg-green text-white font-bold">
+                진행 중
+              </span>
+            ) : (
+              <span className="text-xs p-1 rounded bg-red text-white ">
+                종료
+              </span>
+            )}
+            <span className="text-lg font-bold">{mockSurvey.title}</span>
+          </div>
+          <div className="flex gap-2 items-center text-gray-500">
+            <span>담당자 {mockSurvey.writer}</span>
+            <span className="text-gray-300">|</span>
+            <span>{mockSurvey.time} 설문</span>
+          </div>
+          <div className="flex flex-col">
+            <span className="font-bold">✨ 어떤 보상을 받나요?</span>
+            <span>{mockSurvey.reward}</span>
+          </div>
+          <a
+            href="https://forms.gle/64UkrJpRHHLdJQwi9"
+            target="_blank"
+            rel="noreferrer"
+            className="flex justify-center p-2 rounded bg-blue text-white font-bold"
+          >
+            설문조사 이동하기
+          </a>
+        </div>
+
+        <p className="flex flex-col py-4">
+          {mockSurvey.contents.split('\n').map((line, index) => (
+            <span key={index}>
+              {line}
+              <br />
+            </span>
+          ))}
+        </p>
+      </MainLayout>
+    </Layout>
   );
 };
