02d65d648ff4d6cacf3cc6a870a9fbc18f743067
Yeonu Kim <88171936+Yeonu-Kim@users.noreply.github.com>
2024-12-26 00:01:47 +0900

    ➕ 테일윈드 설치 및 환경 설정 (#10)
    ### 📝 작업 내용
    
    - tailwind, tw-merge, clsx, cva를 설치하여 테일윈드 css 환경을 생성하였습니다.
    - 디자인 시스템이 적용된 버튼 컴포넌트를 생성하였습니다.
    
    ### 📸 스크린샷 (선택)
    
    ### 🚀 리뷰 요구사항 (선택)

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      blue: '#1fb6ff',
+      purple: '#7e5bef',
+      pink: '#ff49db',
+      orange: '#ff7849',
+      green: '#13ce66',
+      yellow: '#ffc82c',
+      gray: '#8492a6',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
