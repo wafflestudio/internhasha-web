c3929433edfe58e7df19d14457c8345e12254603
Yeonu-Kim <ywk0524@snu.ac.kr>
2024-12-29 22:16:49 +0900

    🐛 parsing error 대응

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import react from '@woohm402/eslint-config-react';
 
 export default [
   {
-    ignores: ['.yarn', '*.js'],
+    ignores: ['.yarn', '*.js', 'public/mockServiceWorker.js'],
   },
   ...react({
     tsconfigRootDir: import.meta.dirname,
