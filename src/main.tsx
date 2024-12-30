e5a3db71cfa787dad06c965a703d16378573a713
Yeonu Kim <88171936+Yeonu-Kim@users.noreply.github.com>
2024-12-30 15:18:39 +0900

    ✨ 목 API 생성 (#36)
    ### 📝 작업 내용
    
    - msw를 설치하였습니다.
    - 목 api에 반응하도록 설정하였습니다.
    - request, response 타입을 schema에서 따로 설정할 수 있도록 조성하였습니다.

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,37 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 
-import { App } from './App';
+import { App } from '@/App';
+import { EnvContext } from '@/shared/context/EnvContext';
 
 const root = document.getElementById('root');
 
 if (root === null) throw new Error('Root element not found');
 
-createRoot(root).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-);
+const ENV = {
+  APP_ENV: import.meta.env.MODE as 'prod' | 'dev' | 'mock',
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
+};
+
+async function enableMocking() {
+  if (ENV.APP_ENV === 'mock') {
+    const { worker } = await import('@/mocks');
+
+    return worker.start({
+      onUnhandledRequest: 'bypass',
+    });
+  }
+  return;
+}
+
+void enableMocking().then(() => {
+  createRoot(root).render(
+    <StrictMode>
+      <EnvContext.Provider value={ENV}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </EnvContext.Provider>
+    </StrictMode>,
+  );
+});
---
+++ src/mocks/handlers.ts
@@ -0,0 +1,3 @@
+import { echoHandlers } from '@/mocks/echo/handlers';
+
+export const handlers = [...echoHandlers];
