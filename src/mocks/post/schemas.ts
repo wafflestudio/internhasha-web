a3729cdf8d526ee84205760456f6cfc6c48d7fc7
goranikin <goranikin@snu.ac.kr>
2025-01-06 19:18:45 +0900

    바뀐 타입에 맞게 mock api 수정 및 mock 데이터 생성

--- src/mocks/post/schemas.ts
+++ src/mocks/post/schemas.ts
@@ -1,5 +1,27 @@
-export type PostsResponse = {
+
+export type Post = {
   id: string;
   name: string;
-  description: string;
+  title: string;
+  email: string;
+  author: {
+    id: string,
+    username: string,
+    profileImageLink?: string
+  },
+  tags: string[],
+  roles: {
+    id: string,
+    category: string,
+    detail: string,
+    headcount: number
+  }[],
+  imageLink: string,
+  investAmount: string,
+  investCompany: string,
+  isActive: boolean,
+};
+
+export type PostsResponse = {
+  posts: Post[];
 };
