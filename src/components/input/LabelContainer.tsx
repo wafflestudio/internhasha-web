df81bb8119833516e76e20d46f2826e951ae2bd1
Yeonu Kim <88171936+Yeonu-Kim@users.noreply.github.com>
2025-01-07 12:58:13 +0900

    ✨ 화면 명세 중 로컬 회원가입 조건 반영 (#64)
    ### 📝 작업 내용
    
    로컬 회원가입 조건을 반영합니다.
    
    ### 📸 스크린샷 (선택)
    - 중복확인 완료 시 성공 및 실패 문구가 나타나도록 합니다.
    - 비밀번호 클릭 시 조건이 나타날 수 있도록 합니다.
    - 조건은 입력하는대로 변화하도록 합니다.
    - 이때 비밀번호 input에서 focus를 해제하면 조건이 나타나지 않습니다.
    - 비밀번호 확인 input에 한 개 이상의 문자가 들어가면 일치 여부를 확인하는 문구가 하단에 나타납니다.
    - 회원가입하기 버튼을 눌렀을 때 에러는 하단에 나타나도록 합니다.
    - 작성 완료 시 이메일 인증 페이지로 이동합니다.
    
    ### 🚀 리뷰 요구사항 (선택)
    
    - 유저 플로우가 맞는지 다시 한 번 확인해주시기 바랍니다.

--- src/components/input/LabelContainer.tsx
+++ src/components/input/LabelContainer.tsx
@@ -1,25 +1,16 @@
 interface LabelContainertProps extends React.HTMLAttributes<HTMLDivElement> {
   label: string;
-  isError?: boolean;
-  description?: string;
 }
 
 export const LabelContainer = ({
   children,
   label,
   id,
-  isError,
-  description,
 }: LabelContainertProps) => {
   return (
     <div>
       <label htmlFor={id}>{label}</label>
       {children}
-      {description !== undefined && isError === true && (
-        <div style={{ marginTop: '20px', fontSize: '12px', color: 'black' }}>
-          <strong>{description}</strong>
-        </div>
-      )}
     </div>
   );
 };
