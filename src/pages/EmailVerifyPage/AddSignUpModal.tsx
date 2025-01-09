6905d1b21727da6658fb419ae0e3b3fd7cdc1cb9
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-01-09 14:32:17 +0900

    ✨ 이메일 중복 시 모달 나타나도록 설정

--- src/pages/EmailVerifyPage/AddSignUpModal.tsx
+++ src/pages/EmailVerifyPage/AddSignUpModal.tsx
@@ -1,4 +1,5 @@
 import { useMutation } from '@tanstack/react-query';
+import { useState } from 'react';
 
 import { Button } from '@/components/button';
 import { useGuardContext } from '@/shared/context/hooks';
@@ -22,70 +23,134 @@ type AddLocalSignUpModalProps = {
 };
 
 export const AddGoogleSignUpModal = ({ body }: AddGoogleSignUpModalProps) => {
+  const [responseMessage, setResponseMessage] = useState<string | null>(null);
   const { toSignInSelect } = useRouteNavigation();
-  const handleClickAddGoogleSignUpButton = () => {};
+  const { addGoogleSignUp, isPending } = useAddGoogleSignUp({
+    setResponseMessage,
+  });
+  const handleClickAddGoogleSignUpButton = () => {
+    addGoogleSignUp(body);
+  };
 
   return (
     <div>
       <p>이미 회원가입 내역이 존재합니다.</p>
       <p>구글 로그인을 추가하시겠습니까?</p>
       <div>
-        <Button onClick={toSignInSelect}>로그인하러 가기</Button>
-        <Button onClick={handleClickAddGoogleSignUpButton}>
+        <Button onClick={toSignInSelect} disabled={isPending}>
+          로그인하러 가기
+        </Button>
+        <Button onClick={handleClickAddGoogleSignUpButton} disabled={isPending}>
           구글 로그인 추가하기
         </Button>
       </div>
+      {responseMessage !== null && <div>{responseMessage}</div>}
     </div>
   );
 };
 
 export const AddLocalSignUpModal = ({ body }: AddLocalSignUpModalProps) => {
+  const [responseMessage, setResponseMessage] = useState<string | null>(null);
   const { toSignInSelect } = useRouteNavigation();
-  const handleClickAddLocalSignUpButton = () => {};
+  const { addLocalSignUp, isPending } = useAddLocalSignUp({
+    setResponseMessage,
+  });
+  const handleClickAddLocalSignUpButton = () => {
+    addLocalSignUp(body);
+  };
 
   return (
     <div>
       <p>이미 회원가입 내역이 존재합니다.</p>
       <p>아이디/비밀번호 로그인을 추가하시겠습니까?</p>
       <div>
-        <Button onClick={toSignInSelect}>로그인하러 가기</Button>
-        <Button onClick={handleClickAddLocalSignUpButton}>
+        <Button onClick={toSignInSelect} disabled={isPending}>
+          로그인하러 가기
+        </Button>
+        <Button onClick={handleClickAddLocalSignUpButton} disabled={isPending}>
           로컬 로그인 추가
         </Button>
       </div>
+      {responseMessage !== null && <div>{responseMessage}</div>}
     </div>
   );
 };
 
-// const useAddGoogleSignUp = () => {
-//   const { authService } = useGuardContext(ServiceContext);
+const useAddGoogleSignUp = ({
+  setResponseMessage,
+}: {
+  setResponseMessage(input: string): void;
+}) => {
+  const { authService } = useGuardContext(ServiceContext);
+  const { toSignUpComplete } = useRouteNavigation();
 
-//   const { mutate: sendCode, isPending } = useMutation({
-//     mutationFn: ({ snuMail }: { snuMail: string }) => {
-//       return authService.sendEmailCode({
-//         snuMail,
-//       });
-//     },
-//     onSuccess: (response) => {
-//       if (response.type === 'success') {
-//       } else {
-//       }
-//     },
-//     onError: () => {
-//       setResponseMessage(
-//         '코드 전송에 실패했습니다. 잠시 후에 다시 실행해주세요.',
-//       );
-//       setSendSuccess(false);
-//       stopTimer();
-//     },
-//   });
+  const { mutate: addGoogleSignUp, isPending } = useMutation({
+    mutationFn: ({ snuMail, token }: { snuMail: string; token: string }) => {
+      return authService.addGoogleSignUp({ snuMail, googleAccessToken: token });
+    },
+    onSuccess: (response) => {
+      if (response.type === 'success') {
+        toSignUpComplete();
+      } else {
+        setResponseMessage(response.message);
+      }
+    },
+    onError: () => {
+      setResponseMessage(
+        '구글 회원가입 추가에 실패했습니다. 잠시 후에 다시 실행해주세요.',
+      );
+    },
+  });
 
-//   return {
-//     sendCode,
-//     sendSuccess,
-//     isCodeExpired,
-//     timeLeft,
-//     responseMessage,
-//     isPending,
-//   };
-// };
+  return {
+    addGoogleSignUp,
+    isPending,
+  };
+};
+
+const useAddLocalSignUp = ({
+  setResponseMessage,
+}: {
+  setResponseMessage(input: string): void;
+}) => {
+  const { authService } = useGuardContext(ServiceContext);
+  const { toSignUpComplete } = useRouteNavigation();
+
+  const { mutate: addLocalSignUp, isPending } = useMutation({
+    mutationFn: ({
+      snuMail,
+      localId,
+      password,
+      username,
+    }: {
+      snuMail: string;
+      localId: string;
+      password: string;
+      username: string;
+    }) => {
+      return authService.addLocalSignUp({
+        snuMail,
+        localId,
+        password,
+        username,
+      });
+    },
+    onSuccess: (response) => {
+      if (response.type === 'success') {
+        toSignUpComplete();
+      } else {
+        setResponseMessage(response.message);
+      }
+    },
+    onError: () => {
+      setResponseMessage(
+        '로컬 회원가입 추가에 실패했습니다. 잠시 후에 다시 실행해주세요.',
+      );
+    },
+  });
+
+  return {
+    addLocalSignUp,
+    isPending,
+  };
+};
--- src/pages/EmailVerifyPage/EmailVerifyForm.tsx
+++ src/pages/EmailVerifyPage/EmailVerifyForm.tsx
@@ -14,6 +14,8 @@ import { ServiceContext } from '@/shared/context/ServiceContext';
 import { useRouteNavigation } from '@/shared/route/useRouteNavigation';
 import { formatNumberToTime } from '@/util/format';
 
+import { AddGoogleSignUpModal, AddLocalSignUpModal } from './AddSignUpModal';
+
 type VerifyMailBody =
   | {
       authProvider: 'GOOGLE';
@@ -36,9 +38,7 @@ export const EmailVerifyForm = () => {
 
   const { toSignUpLocal, toSignUpSelect } = useRouteNavigation();
   const [showSendCodeError, setShowSendCodeError] = useState(false);
-  const [showModal, setShowModal] = useState<'NONE' | 'GOOGLE' | 'LOCAL'>(
-    'NONE',
-  );
+  const [showModal, setShowModal] = useState(false);
   const { snuMail, code } = authPresentation.useValidator({});
   const {
     sendCode,
@@ -205,7 +205,21 @@ export const EmailVerifyForm = () => {
           </SubmitButton>
         </div>
       </FormContainer>
-      {showModal !== 'NONE' && <div>모달</div>}
+      {showModal && body.authProvider === 'GOOGLE' && (
+        <AddGoogleSignUpModal
+          body={{ token: body.token, snuMail: snuMail.postfix }}
+        />
+      )}
+      {showModal && body.authProvider === 'LOCAL' && (
+        <AddLocalSignUpModal
+          body={{
+            localId: body.localId,
+            password: body.password,
+            username: body.username,
+            snuMail: snuMail.postfix,
+          }}
+        />
+      )}
     </>
   );
 };
@@ -349,7 +363,7 @@ const useEmailVerify = () => {
 const useGoogleSignUp = ({
   setShowModal,
 }: {
-  setShowModal(input: 'NONE' | 'GOOGLE' | 'LOCAL'): void;
+  setShowModal(input: boolean): void;
 }) => {
   const { authService } = useGuardContext(ServiceContext);
   const [responseMessage, setResponseMessage] = useState('');
@@ -370,7 +384,7 @@ const useGoogleSignUp = ({
           response.status === 409 &&
           response.message === '동일한 스누메일로 등록된 계정이 존재합니다.'
         ) {
-          setShowModal('GOOGLE');
+          setShowModal(true);
         }
         setResponseMessage(response.message);
       }
@@ -388,7 +402,7 @@ const useGoogleSignUp = ({
 const useLocalSignUp = ({
   setShowModal,
 }: {
-  setShowModal(input: 'NONE' | 'GOOGLE' | 'LOCAL'): void;
+  setShowModal(input: boolean): void;
 }) => {
   const { authService } = useGuardContext(ServiceContext);
   const [responseMessage, setResponseMessage] = useState('');
@@ -416,7 +430,7 @@ const useLocalSignUp = ({
           response.status === 409 &&
           response.message === '동일한 스누메일로 등록된 계정이 존재합니다.'
         ) {
-          setShowModal('LOCAL');
+          setShowModal(true);
         }
         setResponseMessage(response.message);
       }
