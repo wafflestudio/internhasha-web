d1bd05752bda79476432acbaf7a0a09f1f563121
goranikin <goranikin@snu.ac.kr>
2025-01-09 10:07:13 +0900

    타입에 맞게 mockPosts 수정 및 data 파일로 옮김

---
+++ src/mocks/post/data.ts
@@ -0,0 +1,166 @@
+export const mockPosts = [
+  {
+    id: '1',
+    companyName: 'ABC 회사',
+    title: 'string',
+    email: 'string',
+    author: {
+      id: 'string',
+      name: 'string',
+      profileImageLink: 'String?',
+    },
+    explanation: '회사 설명입니다.', // 추가
+    tags: ['ABC초콜릿', 'ABC초코쿠키도 있어요', '설빙ABC초코쿠키도 많은관심'],
+    roles: [
+      {
+        id: 'asdjfkadf',
+        category: '개발자',
+        detail: '프론트엔드',
+        headcount: '3', // 문자열로 수정
+      },
+      {
+        id: 'sjdfhsjldf',
+        category: '개발자',
+        detail: 'SpringBoot',
+        headcount: '2', // 문자열로 수정
+      },
+    ],
+    imageLink: '목업',
+    investAmount: 12345,
+    investCompany: ['String'], // 배열로 수정
+    isActive: true,
+    employmentEndDate: new Date('2023-12-31'), // 추가
+  },
+  {
+    id: '2',
+    companyName: 'ABC 회사',
+    title: 'string',
+    email: 'string',
+    author: {
+      id: 'string',
+      name: 'string',
+      profileImageLink: 'String?',
+    },
+    explanation: '회사 설명입니다.', // 추가
+    tags: ['ABC초콜릿', 'ABC초코쿠키도 있어요', '설빙ABC초코쿠키도 많은관심'],
+    roles: [
+      {
+        id: 'asdjfkadf',
+        category: '개발자',
+        detail: '프론트엔드',
+        headcount: '3', // 문자열로 수정
+      },
+      {
+        id: 'sjdfhsjldf',
+        category: '개발자',
+        detail: 'SpringBoot',
+        headcount: '2', // 문자열로 수정
+      },
+    ],
+    imageLink: '목업',
+    investAmount: 12345,
+    investCompany: ['String'], // 배열로 수정
+    isActive: true,
+    employmentEndDate: new Date('2023-12-31'), // 추가
+  },
+  {
+    id: '3',
+    companyName: 'ABC 회사',
+    title: 'string',
+    email: 'string',
+    author: {
+      id: 'string',
+      name: 'string',
+      profileImageLink: 'String?',
+    },
+    explanation: '회사 설명입니다.', // 추가
+    tags: ['ABC초콜릿', 'ABC초코쿠키도 있어요', '설빙ABC초코쿠키도 많은관심'],
+    roles: [
+      {
+        id: 'asdjfkadf',
+        category: '개발자',
+        detail: '프론트엔드',
+        headcount: '3', // 문자열로 수정
+      },
+      {
+        id: 'sjdfhsjldf',
+        category: '개발자',
+        detail: 'SpringBoot',
+        headcount: '2', // 문자열로 수정
+      },
+    ],
+    imageLink: '목업',
+    investAmount: 12345,
+    investCompany: ['String'], // 배열로 수정
+    isActive: true,
+    employmentEndDate: new Date('2023-12-31'), // 추가
+  },
+  {
+    id: '4',
+    companyName: 'ABC 회사',
+    title: 'string',
+    email: 'string',
+    author: {
+      id: 'string',
+      name: 'string',
+      profileImageLink: 'String?',
+    },
+    explanation: '회사 설명입니다.', // 추가
+    tags: ['ABC초콜릿', 'ABC초코쿠키도 있어요', '설빙ABC초코쿠키도 많은관심'],
+    roles: [
+      {
+        id: 'asdjfkadf',
+        category: '개발자',
+        detail: '프론트엔드',
+        headcount: '3', // 문자열로 수정
+      },
+      {
+        id: 'sjdfhsjldf',
+        category: '개발자',
+        detail: 'SpringBoot',
+        headcount: '2', // 문자열로 수정
+      },
+    ],
+    imageLink: '목업',
+    investAmount: 12345,
+    investCompany: ['String'], // 배열로 수정
+    isActive: true,
+    employmentEndDate: new Date('2023-12-31'), // 추가
+  },
+  {
+    id: '5',
+    companyName: 'ABC 회사',
+    title: 'string',
+    email: 'string',
+    author: {
+      id: 'string',
+      name: 'string',
+      profileImageLink: 'String?',
+    },
+    explanation: '회사 설명입니다.', // 추가
+    tags: ['ABC초콜릿', 'ABC초코쿠키도 있어요', '설빙ABC초코쿠키도 많은관심'],
+    roles: [
+      {
+        id: 'asdjfkadf',
+        category: '개발자',
+        detail: '프론트엔드',
+        headcount: '3', // 문자열로 수정
+      },
+      {
+        id: 'sjdfhsjldf',
+        category: '개발자',
+        detail: 'SpringBoot',
+        headcount: '2', // 문자열로 수정
+      },
+    ],
+    imageLink: '목업',
+    investAmount: 12345,
+    investCompany: ['String'], // 배열로 수정
+    isActive: true,
+    employmentEndDate: new Date('2023-12-31'), // 추가
+  }
+];
+
+export const mockPost = {
+
+}
--- src/mocks/post/resolvers.ts
+++ src/mocks/post/resolvers.ts
@@ -1,5 +1,6 @@
 import { HttpResponse, type HttpResponseResolver } from 'msw';
 
+import { mockPosts } from '@/mocks/post/data.ts';
 import type { PostsResponse } from '@/mocks/post/schemas.ts';
 
 type PostsResolver = {
@@ -8,163 +9,10 @@ type PostsResolver = {
 
 export const postsResolver: PostsResolver = {
   posts: () => {
-    const posts = {
-      posts: mockPostData,
-    };
+    const posts = mockPosts
 
     return HttpResponse.json(posts, { status: 200 });
   },
 };
 
-const mockPostData = [
-  {
-    id: '1',
-    name: 'ABC 회사',
-    title: 'string',
-    email: 'string',
-    author: {
-      id: 'string',
-      username: 'string',
-      profileImageLink: 'String?',
-    },
-    tags: ['ABC초콜릿', 'ABC초코쿠키도 있어요', '설빙ABC초코쿠키도 많은관심'],
-    roles: [
-      {
-        id: 'asdjfkadf',
-        category: '개발자',
-        detail: '프론트엔드',
-        headcount: 3,
-      },
-      {
-        id: 'sjdfhsjldf',
-        category: '개발자',
-        detail: 'SpringBoot',
-        headcount: 2,
-      },
-    ],
-    imageLink: '목업',
-    investAmount: 'string',
-    investCompany: 'String',
-    isActive: true,
-  },
-  {
-    id: '2',
-    name: 'ABC 회사',
-    title: 'string',
-    email: 'string',
-    author: {
-      id: 'string',
-      username: 'string',
-      profileImageLink: 'String?',
-    },
-    tags: ['ABC초콜릿', 'ABC초코쿠키도 있어요', '설빙ABC초코쿠키도 많은관심'],
-    roles: [
-      {
-        id: 'asdjfkadf',
-        category: '개발자',
-        detail: '프론트엔드',
-        headcount: 3,
-      },
-      {
-        id: 'sjdfhsjldf',
-        category: '개발자',
-        detail: 'SpringBoot',
-        headcount: 2,
-      },
-    ],
-    imageLink: '목업',
-    investAmount: 'string',
-    investCompany: 'String',
-    isActive: true,
-  },
-  {
-    id: '3',
-    name: 'ABC 회사',
-    title: 'string',
-    email: 'string',
-    author: {
-      id: 'string',
-      username: 'string',
-      profileImageLink: 'String?',
-    },
-    tags: ['ABC초콜릿', 'ABC초코쿠키도 있어요', '설빙ABC초코쿠키도 많은관심'],
-    roles: [
-      {
-        id: 'asdjfkadf',
-        category: '개발자',
-        detail: '프론트엔드',
-        headcount: 3,
-      },
-      {
-        id: 'sjdfhsjldf',
-        category: '개발자',
-        detail: 'SpringBoot',
-        headcount: 2,
-      },
-    ],
-    imageLink: '목업',
-    investAmount: 'string',
-    investCompany: 'String',
-    isActive: true,
-  },
-  {
-    id: '4',
-    name: 'ABC 회사',
-    title: 'string',
-    email: 'string',
-    author: {
-      id: 'string',
-      username: 'string',
-      profileImageLink: 'String?',
-    },
-    tags: ['ABC초콜릿', 'ABC초코쿠키도 있어요', '설빙ABC초코쿠키도 많은관심'],
-    roles: [
-      {
-        id: 'asdjfkadf',
-        category: '개발자',
-        detail: '프론트엔드',
-        headcount: 3,
-      },
-      {
-        id: 'sjdfhsjldf',
-        category: '개발자',
-        detail: 'SpringBoot',
-        headcount: 2,
-      },
-    ],
-    imageLink: '목업',
-    investAmount: 'string',
-    investCompany: 'String',
-    isActive: true,
-  },
-  {
-    id: '5',
-    name: 'ABC 회사',
-    title: 'string',
-    email: 'string',
-    author: {
-      id: 'string',
-      username: 'string',
-      profileImageLink: 'String?',
-    },
-    tags: ['ABC초콜릿', 'ABC초코쿠키도 있어요', '설빙ABC초코쿠키도 많은관심'],
-    roles: [
-      {
-        id: 'asdjfkadf',
-        category: '개발자',
-        detail: '프론트엔드',
-        headcount: 3,
-      },
-      {
-        id: 'sjdfhsjldf',
-        category: '개발자',
-        detail: 'SpringBoot',
-        headcount: 2,
-      },
-    ],
-    imageLink: '목업',
-    investAmount: 'string',
-    investCompany: 'String',
-    isActive: true,
-  },
-];
+
