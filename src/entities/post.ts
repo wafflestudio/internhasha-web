da6cb4842a5a313cadd7d4aedbe1fc3004ca92e5
goranikin <goranikin@snu.ac.kr>
2025-01-11 12:34:07 +0900

    [REFACTOROR] 랜딩페이지와 필터링 기능들의 코드르 정리 (#107)
    ### 📝 작업 내용
    
    - type은 entity 폴더로 옮겼습니다.
    - filter 기능에 쓰이는 state를 묶어서 FilterModal에 전달합니다.
    
    ### 📸 스크린샷 (선택)
    
    ### 🚀 리뷰 요구사항 (선택)
    
    - 리뷰어가 특별히 봐주었으면 하는 부분이 있다면 작성해주세요.

---
+++ src/entities/post.ts
@@ -0,0 +1,27 @@
+// filter
+
+export type RoleCategory =
+  | 'PLANNER'
+  | 'FRONT'
+  | 'APP'
+  | 'BACKEND'
+  | 'DESIGN'
+  | 'DATA'
+  | 'MARKETER';
+
+export const ROLE_CATEGORY_LIST: RoleCategory[] = [
+  'PLANNER',
+  'FRONT',
+  'APP',
+  'BACKEND',
+  'DESIGN',
+  'DATA',
+  'MARKETER',
+];
+
+export type FilterElements = {
+  roles?: RoleCategory[];
+  investment?: number;
+  investor?: string;
+  pathStatus?: 0 | 1 | 2;
+};
