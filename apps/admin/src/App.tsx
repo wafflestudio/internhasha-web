b01191ae54199b16c16d85ba912f49fd94f46db3
Yeonu Kim <88171936+Yeonu-Kim@users.noreply.github.com>
2025-01-14 11:08:28 +0900

    ✨ 디자인 시스템 패키지 생성 (#137)
    ### 📝 작업 내용
    
    - 디자인 시스템을 위한 패키지를 루트에 설치합니다.
    - 일단 버튼 컴포넌트만 이동하였으며, 추후에 대부분의 컴포넌트를 해당 패키지로 이동합니다.
    - 다음과 같이 사용합니다.
    ``` tsx
    import { Button } from '@waffle/design-system';
    import { useReducer } from 'react';
    
    export const App = () => {
      const [count, increment] = useReducer((c: number) => c + 1, 0);
    
      return (
        <div>
          <button onClick={increment}>{count}</button>
          <Button>와플의 버튼</Button>
        </div>
      );
    };
    
    ```

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,6 +1,6 @@
 import './reset.css';
 
-import { add, subtract } from '@waffle/math';
+import { Button } from '@waffle/design-system';
 import { useReducer } from 'react';
 
 export const App = () => {
@@ -8,9 +8,8 @@ export const App = () => {
 
   return (
     <div>
-      <p>{add(2, 3)}</p>
-      <p>{subtract(5, 3)}</p>
       <button onClick={increment}>{count}</button>
+      <Button>와플의 버튼</Button>
     </div>
   );
 };
