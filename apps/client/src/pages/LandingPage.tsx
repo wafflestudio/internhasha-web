6a90fcc6d25def03113c3957d28560fc76a44dbb
Yeonu Kim <88171936+Yeonu-Kim@users.noreply.github.com>
2025-01-28 13:44:33 +0900

    ✨ 랜딩페이지 스켈레톤 로딩 추가 (#239)
    ### 📝 작업 내용
    
    - 랜딩페이지에 스켈레톤 로딩을 추가했습니다.

--- apps/client/src/pages/LandingPage.tsx
+++ apps/client/src/pages/LandingPage.tsx
@@ -1,16 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 
 import { SignInForBookmarkModal } from '@/components/modal/SignInForBookmarkModal';
 import { GlobalNavigationBar } from '@/components/nav/GlobarNavigationBar';
 import type { FilterElements, JobMinorCategory } from '@/entities/post';
+import type { Series } from '@/entities/post';
 import {
   FilterSection,
   NarrowRolesFilter,
   PaginationBar,
   PostCard,
   RolesFilter,
-  useGetPosts,
 } from '@/feature/landing';
+import { SkeletonPostCard } from '@/feature/landing/ui/SkeletonPostCard';
+import { useGuardContext } from '@/shared/context/hooks';
+import { ServiceContext } from '@/shared/context/ServiceContext';
 import { useRouteNavigation } from '@/shared/route/useRouteNavigation';
 
 export const LandingPage = () => {
@@ -35,16 +39,12 @@ export const LandingPage = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const [currentGroup, setCurrentGroup] = useState(0);
 
-  const { data: postsData } = useGetPosts({
+  const { postsData } = useGetPosts({
     page: currentPage,
     ...filterElements,
   });
 
-  if (postsData === undefined) {
-    return <p>로딩 중...</p>;
-  }
-
-  const TOTAL_PAGES = postsData.paginator.lastPage;
+  const TOTAL_PAGES = postsData?.paginator.lastPage;
   const PAGES_PER_GROUP = 5;
 
   return (
@@ -56,7 +56,7 @@ export const LandingPage = () => {
         {/* 메인 컨텐츠 */}
         <div className="flex flex-col w-full sm:w-screen-sm md:w-screen-md lg:w-screen-lg xl:max-w-screen-xl md:flex-row m-auto px-2 py-10 gap-2">
           {/* RolesFilter */}
-          <div className="hidden md:block md:flex-col order-1 md:order-none">
+          <div className="hidden md:block md:flex-col order-1 md:order-none md:mt-[58px]">
             <RolesFilter
               roles={filterElements.roles}
               onChangeRoles={handleRolesChange}
@@ -72,9 +72,10 @@ export const LandingPage = () => {
           </div>
 
           {/* 게시글 리스트 및 상단 필터 */}
-          <div className="flex-1 order-2 lg:order-none">
+          <div className="flex-1 order-2 px-2 lg:order-none">
+            <h2 className="text-2xl font-bold mb-6">인턴 공고</h2>
             {/* 상단 필터 섹션 */}
-            <div className="flex justify-between items-center mb-2 pl-2">
+            <div className="flex justify-between items-center py-2">
               <FilterSection
                 filterElements={filterElements}
                 onChangeFilters={setFilterElements}
@@ -83,34 +84,40 @@ export const LandingPage = () => {
 
             {/* 게시글 리스트 */}
             <main>
-              <div className="grid w-full grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 pr-2">
-                {postsData.posts.map((post, idx) => (
-                  <PostCard
-                    key={`post-${idx}`}
-                    post={post}
-                    onDetailClick={(postId) => {
-                      toPost({ postId });
-                    }}
-                    setShowSignInModal={setShowSignInModal}
-                  />
-                ))}
+              <div className="grid w-full grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+                {postsData !== undefined
+                  ? postsData.posts.map((post, idx) => (
+                      <PostCard
+                        key={`post-${idx}`}
+                        post={post}
+                        onDetailClick={(postId) => {
+                          toPost({ postId });
+                        }}
+                        setShowSignInModal={setShowSignInModal}
+                      />
+                    ))
+                  : Array.from({ length: 10 }).map((_, index) => (
+                      <SkeletonPostCard key={index} />
+                    ))}
               </div>
             </main>
 
             {/* 페이지네이션 */}
             <footer className="mt-6 flex justify-center">
-              <PaginationBar
-                totalPages={TOTAL_PAGES}
-                pagesPerGroup={PAGES_PER_GROUP}
-                currentPage={currentPage}
-                currentGroup={currentGroup}
-                onChangePage={(page) => {
-                  setCurrentPage(page);
-                }}
-                onChangeGroup={(group) => {
-                  setCurrentGroup(group);
-                }}
-              />
+              {TOTAL_PAGES !== undefined && (
+                <PaginationBar
+                  totalPages={TOTAL_PAGES}
+                  pagesPerGroup={PAGES_PER_GROUP}
+                  currentPage={currentPage}
+                  currentGroup={currentGroup}
+                  onChangePage={(page) => {
+                    setCurrentPage(page);
+                  }}
+                  onChangeGroup={(group) => {
+                    setCurrentGroup(group);
+                  }}
+                />
+              )}
             </footer>
           </div>
         </div>
@@ -118,4 +125,51 @@ export const LandingPage = () => {
       {showSignInModal && <SignInForBookmarkModal onClose={closeSignInModal} />}
     </>
   );
+};
+
+export const useGetPosts = ({
+  page = 0,
+  roles,
+  investmentMax,
+  investmentMin,
+  series,
+  pathStatus,
+}: {
+  page?: number;
+  roles?: string[];
+  investmentMax?: number;
+  investmentMin?: number;
+  series?: Series[];
+  pathStatus?: number;
+}) => {
+  const { postService } = useGuardContext(ServiceContext);
+
+  const { data: postsData } = useQuery({
+    queryKey: [
+      'postService',
+      'getPosts',
+      page,
+      roles,
+      investmentMax,
+      investmentMin,
+      series,
+      pathStatus,
+    ],
+    queryFn: async () => {
+      const response = await postService.getPosts({
+        page,
+        roles,
+        investmentMax,
+        investmentMin,
+        series,
+        pathStatus,
+      });
+      if (response.type === 'success') {
+        return response.data;
+      }
+      throw new Error('회사 정보를 가져오는데 실패했습니다.');
+    },
+  });
+
+  return { postsData };
 };
