411ee1af13c7f742868802d681ed2ee950cf38f3
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-01-30 23:06:20 +0900

    ➕ 달력 ui 추가

--- apps/client/eslint.config.js
+++ apps/client/eslint.config.js
@@ -1,3 +1,8 @@
 import { reactConfig } from '@waffle/eslint-react';
 
-export default [...reactConfig(import.meta.dirname)];
+export default [
+  ...reactConfig(import.meta.dirname),
+  {
+    ignores: ['src/components/ui/calendar.tsx'],
+  },
+];
