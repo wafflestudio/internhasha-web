b7c7993d1728db372095546d035cee2f28ad0244
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-01-31 11:22:16 +0900

    💄 달력 스타일링 완료

--- apps/client/src/components/ui/calendar.tsx
+++ apps/client/src/components/ui/calendar.tsx
@@ -15,24 +15,36 @@ function Calendar({
 }: CalendarProps) {
   return (
     <DayPicker
+      formatters={{
+        formatWeekdayName: (day) =>
+          day?.toLocaleDateString('en-US', { weekday: 'short' }),
+        formatCaption: (month) =>
+          month?.toLocaleDateString('ko-KR', {
+            year: 'numeric',
+            month: 'long',
+          }),
+      }}
       showOutsideDays={showOutsideDays}
-      className={cn('p-3 shadow-md', className)}
+      className={cn(
+        'inline-flex w-[340px] justify-center p-[22px] rounded-md shadow-md',
+        className,
+      )}
       classNames={{
         months: 'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0',
         month: 'space-y-4',
-        caption: 'flex w-full px-6 pt-1 relative items-center',
-        caption_label: 'text-sm font-medium',
+        caption: 'flex w-full px-2 pt-1 relative items-center',
+        caption_label: 'text-lg text-grey-dark-active font-semibold ',
         nav: 'space-x-1 flex items-center',
         nav_button: cn(
           buttonVariants({ variant: 'ghost' }),
-          'h-4 w-4 bg-transparent p-0 opacity-50 hover:opacity-100',
+          'h-10 w-10 bg-transparent p-0 hover:opacity-100',
         ),
         nav_button_previous: 'absolute right-10',
-        nav_button_next: 'absolute right-4',
+        nav_button_next: 'absolute right-2',
         table: 'w-full border-collapse space-y-1',
         head_row: 'flex justify-center',
-        head_cell: 'text-grey-normal rounded-md w-8 font-normal text-[0.8rem]',
-        row: 'flex justify-center w-full mt-2',
+        head_cell: 'text-grey-normal rounded-md w-10 font-normal text-xs',
+        row: 'flex justify-center w-full mt-4',
         cell: cn(
           'relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected].day-range-end)]:rounded-r-md',
           props.mode === 'range'
@@ -41,7 +53,7 @@ function Calendar({
         ),
         day: cn(
           buttonVariants({ variant: 'ghost' }),
-          'rounded-full h-8 w-8 p-0 font-normal text-black aria-selected:opacity-100',
+          'rounded-full h-9 w-10 p-0 font-normal text-black aria-selected:opacity-100',
         ),
         day_range_start: 'day-range-start',
         day_range_end: 'day-range-end',
@@ -58,10 +70,10 @@ function Calendar({
       }}
       components={{
         IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn('h-4 w-4', className)} {...props} />
+          <ChevronLeft className={cn('h-10 w-10', className)} {...props} />
         ),
         IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn('h-4 w-4', className)} {...props} />
+          <ChevronRight className={cn('h-10 w-10', className)} {...props} />
         ),
       }}
       {...props}
