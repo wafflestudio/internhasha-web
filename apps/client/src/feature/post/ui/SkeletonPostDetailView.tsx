3d60628c27e704e42ebd45181713b8c07a5beaaf
goranikin <goranikin@snu.ac.kr>
2025-02-01 18:20:12 +0100

    adapt skeleton ui to original view.

--- apps/client/src/feature/post/ui/SkeletonPostDetailView.tsx
+++ apps/client/src/feature/post/ui/SkeletonPostDetailView.tsx
@@ -3,41 +3,38 @@ import { Skeleton } from '@/components/ui/skeleton';
 export const SkeletonPostDetailView = () => {
   return (
     <div className="max-w-screen-md mx-auto gap-12 p-10 flex flex-col md:flex-row">
-      <div className="flex flex-col gap-12 flex-1">
+      <div className="flex flex-col gap-5 flex-1">
         {/* 헤더 */}
         <div className="flex flex-col justify-between items-start gap-2">
           <div className="flex items-center gap-3">
-            <div className="w-10 h-10 bg-gray-200 rounded-lg" />
-            <Skeleton className="h-6 w-32" />
+            <div className="w-10 h-10 bg-gray-200 rounded-lg object-cover" />
+            <Skeleton className="h-6 w-40" />
           </div>
           <div className="flex justify-between w-full">
-            <Skeleton className="h-8 w-40" />
-            <div className="content-center">
-              <Skeleton className="w-[30px] h-[30px] rounded-full" />
-            </div>
+            <Skeleton className="h-12 w-60" />
+            <Skeleton className="w-[30px] h-[30px] rounded-full" />
           </div>
         </div>
 
         {/* 회사 소개 */}
         <div className="flex flex-col gap-10">
-          <div className="border-none flex flex-col gap-5 p-4 rounded-lg">
-            <Skeleton className="h-6 w-full" />
+          <div className="border-none flex flex-col gap-5 p-4 bg-gray-100 rounded-lg">
+            <Skeleton className="h-6 w-32" />
             <Skeleton className="h-5 w-full" />
           </div>
 
           {/* 투자 정보 */}
           <div className="flex flex-col gap-4 space-y-4 py-6 px-4 sm:py-6 sm:px-8 rounded-lg bg-gray-100">
             <div className="flex flex-col gap-8 sm:gap-16 xs:flex-row">
-              <div className="flex items-center flex-1">
+              <div className="flex items-center flex-1 gap-4">
                 <Skeleton className="h-[20px] w-[80px]" />
-                <Skeleton className="h-[30px] w-[100px] ml-4" />
+                <Skeleton className="h-[30px] w-[120px] rounded-full" />
               </div>
-              <div className="flex items-center flex-1">
+              <div className="flex items-center flex-1 gap-4">
                 <Skeleton className="h-[20px] w-[80px]" />
-                <Skeleton className="h-[30px] w-[100px] ml-4" />
+                <Skeleton className="h-[30px] w-[120px] rounded-full" />
               </div>
             </div>
-
             {/* 투자사 */}
             <div className="flex items-center gap-2">
               <Skeleton className="h-[20px] w-[80px]" />
@@ -46,7 +43,6 @@ export const SkeletonPostDetailView = () => {
               ))}
             </div>
           </div>
-
           {/* 회사 홈페이지 / IR Deck 자료 */}
           <section className="flex gap-4 xs:gap-10">
             {[1, 2].map((_, index) => (
@@ -54,10 +50,10 @@ export const SkeletonPostDetailView = () => {
                 key={index}
                 className="flex flex-col gap-5 items-center xs:flex-row"
               >
-                <Skeleton className="h-[20px] w-[120px]" />
+                <Skeleton className="h-[20px] w-[80px]" />
                 <Skeleton
                   key={index}
-                  className="flex items-center px-3 py-2 gap-1 rounded-lg bg-gray-200 h-[40px] w-[150px]"
+                  className="flex items-center px-3 py-2 gap-1 rounded-lg bg-gray-200 h-[40px] w-[80px]"
                 />
               </div>
             ))}
@@ -87,7 +83,7 @@ export const SkeletonPostDetailView = () => {
       </div>
 
       {/* 채용 정보 및 버튼 */}
-      <div className="flex flex-col flex-[0.5] w-full gap-5 my-36 md:w-80">
+      <div className="flex flex-col flex-1 w-full gap-5 my-36">
         {/* 채용 마감일 */}
         <Skeleton className="h-[20px] w-full" />
 
