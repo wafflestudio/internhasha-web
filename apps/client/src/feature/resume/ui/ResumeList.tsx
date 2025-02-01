f3a013ac8d16ef3a9eef71831e2ac90b17c9370c
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-02-01 23:29:32 +0900

    💄 일반 유저 마이페이지 정렬 및 스켈레톤 로딩 추가

--- apps/client/src/feature/resume/ui/ResumeList.tsx
+++ apps/client/src/feature/resume/ui/ResumeList.tsx
@@ -1,50 +1,54 @@
 import { useQuery } from '@tanstack/react-query';
 
-import { ICON_SRC } from '@/entities/asset';
+import { Skeleton } from '@/components/ui/skeleton';
 import { useGuardContext } from '@/shared/context/hooks';
 import { ServiceContext } from '@/shared/context/ServiceContext';
 import { TokenContext } from '@/shared/context/TokenContext';
-import { useRouteNavigation } from '@/shared/route/useRouteNavigation.ts';
-import { getFormatDate } from '@/util/postFormatFunctions.ts';
+import { useRouteNavigation } from '@/shared/route/useRouteNavigation';
+import { getFormatDate } from '@/util/postFormatFunctions';
 
 export const ResumeListView = () => {
   const { resumeListData } = useGetResumeList();
   const { toResumeDetail } = useRouteNavigation();
 
-  if (resumeListData === undefined) {
-    return <div>로딩중...</div>;
-  }
-
-  if (resumeListData.type === 'error') {
+  if (resumeListData?.type === 'error') {
     return (
       <div>정보를 불러오는 중 문제가 발생하였습니다. 새로고침해주세요.</div>
     );
   }
 
-  const { resumeList } = resumeListData.data;
-
   return (
     <div className="flex flex-col w-full gap-3">
-      {resumeList.map((resume) => (
-        <div
-          key={resume.id}
-          className="flex w-full px-[24px] py-[10px] justify-between cursor-pointer hover:shadow-md"
-          onClick={() => {
-            toResumeDetail({ resumeId: resume.id });
-          }}
-        >
-          <div className="flex gap-[18px]">
-            <img src={ICON_SRC.BOOKMARK.SELECTED} />
-            <span className="text-grey-darker">{resume.positionTitle}</span>
-          </div>
-          <div className="flex gap-[18px]">
-            <span className="text-grey-darker">{resume.companyName}</span>
-            <span className="px-2 py-1 bg-grey-darker text-white text-sm">
+      {resumeListData !== undefined ? (
+        resumeListData.data.resumeList.map((resume) => (
+          <div
+            key={resume.id}
+            className="flex px-[24px] h-[50px] justify-between items-center cursor-pointer bg-white rounded-md duration-300 hover:shadow-md"
+            onClick={() => {
+              toResumeDetail({ resumeId: resume.id });
+            }}
+          >
+            <span className="w-[350px] text-grey-darker font-semibold truncate">
+              {resume.positionTitle}
+            </span>
+            <span className="text-sm text-grey-normal">
               {getFormatDate(resume.createdAt)}
             </span>
           </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <>
+          {Array.from({ length: 12 }).map((_, idx) => (
+            <div
+              key={`loading-${idx}`}
+              className="flex px-[24px] h-[50px] justify-between items-center cursor-pointer bg-white rounded-md"
+            >
+              <Skeleton className="w-[350px] h-[18px]" />
+              <Skeleton className="w-[80px] h-6" />
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 };
