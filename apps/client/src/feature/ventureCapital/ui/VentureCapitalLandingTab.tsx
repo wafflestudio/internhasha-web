67fae8938ccee430c52daad3324d8149bf519681
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-02-01 18:03:03 +0900

    💄 관리 기업 탭 바 폰트 세미 볼드로 수정

--- apps/client/src/feature/ventureCapital/ui/VentureCapitalLandingTab.tsx
+++ apps/client/src/feature/ventureCapital/ui/VentureCapitalLandingTab.tsx
@@ -24,7 +24,7 @@ export const VentureCapitalLandingTab = () => {
     >
       <div className="flex flex-col gap-[30px]">
         <div className="flex flex-col gap-4 xs:flex-row w-full xs:justify-between">
-          <TabsList className="flex text-lg font-medium">
+          <TabsList className="flex text-lg font-semibold">
             <TabsTrigger value="COMPANY">관리 기업</TabsTrigger>
             <TabsTrigger value="RESUME">작성한 공고</TabsTrigger>
             <TabsTrigger value="MYPAGE">내 정보</TabsTrigger>
