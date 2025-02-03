5d93e4ab10dab57f2d46e09eefa34829fecf0a4a
Yeonu Kim <88171936+Yeonu-Kim@users.noreply.github.com>
2025-02-04 00:53:55 +0900

    🐛 회원가입 하지 않은 유저에 대해 회원가입 유도 모달 띄우기 (#333)
    ### 📝 작업 내용
    
    - 아이디와 비밀번호가 틀렸을 경우 '아이디 또는 비밀번호가 일치하지 않습니다.'를 띄웁니다.
    - 회원가입하지 않은 유저일 경우 회원가입 유도 모달을 띄웁니다.

---
+++ apps/client/src/components/modal/SignUpModal.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@/components/ui/button';
+import { ModalFloatBackground } from '@/components/ui/layout';
+import { useRouteNavigation } from '@/shared/route/useRouteNavigation';
+
+export const SignUpModal = ({ onClose }: { onClose(): void }) => {
+  const { toSignUpSelect } = useRouteNavigation();
+
+  return (
+    <ModalFloatBackground variant="transparent">
+      <div className="flex flex-col text-center gap-[14px]">
+        <h2 className="text-xl font-bold">아직 회원가입되지 않았습니다!</h2>
+        <p>회원가입 페이지로 이동하시겠습니까?</p>
+      </div>
+      <div className="flex gap-2">
+        <Button variant="secondary" onClick={onClose} className="flex-1">
+          닫기
+        </Button>
+        <Button onClick={toSignUpSelect} className="flex-1">
+          회원가입 페이지로
+        </Button>
+      </div>
+    </ModalFloatBackground>
+  );
+};
