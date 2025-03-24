907498cc5cc973dbd8534869f9cc09206123b97e
sejunPark <aprimelonge@snu.ac.kr>
2025-03-24 23:41:20 +0900

    ✨ 회사 커피챗 상세페이지 api 연결

--- src/feature/coffeeChat/ui/detail/CompanyCoffeeChatDetailView.tsx
+++ src/feature/coffeeChat/ui/detail/CompanyCoffeeChatDetailView.tsx
@@ -1,15 +1,92 @@
-import { ApplicantInfo } from '@/feature/applicant';
+import { useState } from 'react';
 
-export const CoffeeChatDetailView = () => {
+import { FormErrorResponse } from '@/components/response/formResponse';
+import { Button } from '@/components/ui/button';
+import { ApplicantInfo } from '@/feature/applicant';
+import { SkeletonCoffeeChatDetailView } from '@/feature/coffeeChat/ui/detail/SkeletonCoffeeChatDetailView';
+import {
+  useGetCoffeeChatDetail,
+  useUpdateCoffeeChatStatus,
+} from '@/feature/coffeeChat/ui/detail/useCoffeeChatDetailHooks';
+import { useRouteNavigation } from '@/shared/route/useRouteNavigation';
+
+export const CoffeeChatDetailView = ({
+  coffeeChatId,
+}: {
+  coffeeChatId: string;
+}) => {
+  const [responseMessage, setResponseMessage] = useState('');
+  const { coffeeChatDetailData } = useGetCoffeeChatDetail({ coffeeChatId });
+  const { toMyPage } = useRouteNavigation();
+  const { updateCoffeeChatStatus, isPending } = useUpdateCoffeeChatStatus({
+    setResponseMessage,
+  });
+
+  const handleStatusChange = (status: 'ACCEPTED' | 'REJECTED') => {
+    updateCoffeeChatStatus({
+      coffeeChatId,
+      body: { coffeeChatStatus: status },
+    });
+  };
+
+  if (coffeeChatDetailData === undefined) {
+    return <SkeletonCoffeeChatDetailView />;
+  }
+
+  if (coffeeChatDetailData.type === 'error') {
+    return (
+      <div>정보를 불러오는 중 문제가 발생하였습니다. 새로고침해주세요.</div>
+    );
+  }
+  const coffeeChatDetail = coffeeChatDetailData.data;
+  const isWaiting = coffeeChatDetail.coffeeChatStatus === 'WAITING';
+
   return (
-    <div className="mx-auto flex w-full flex-col gap-10 px-6 py-[30px] sm:w-screen-sm md:w-screen-md lg:w-screen-lg xl:max-w-screen-xl">
-      <div className="flex w-[700px] flex-col gap-6 rounded-lg bg-white px-[40px] py-[46px] text-grey-900">
-        <ApplicantInfo />
-        <div className="flex flex-col gap-[16px]">
-          <h3 className="text-22 font-bold">커피챗 내용</h3>
-          <p className="text-sm text-gray-700">커피챗 내용 test</p>
+    <>
+      <div className="mx-auto flex w-full gap-[90px] px-6 py-[30px] sm:w-screen-sm md:w-screen-md lg:w-screen-lg xl:max-w-screen-xl">
+        <div className="flex w-[700px] flex-col gap-6 rounded-lg bg-white px-[40px] py-[46px] text-grey-900">
+          <ApplicantInfo />
+          <div className="flex flex-col gap-[16px]">
+            <h3 className="text-22 font-bold">커피챗 신청 내용</h3>
+            <p className="text-sm text-gray-700">{coffeeChatDetail.content}</p>
+          </div>
+        </div>
+        <div className="mb-[86px] mt-[963px] flex flex-col gap-[8px]">
+          <Button
+            variant="default"
+            onClick={() => {
+              handleStatusChange('ACCEPTED');
+            }}
+            className="w-[292px]"
+            disabled={isPending || !isWaiting}
+          >
+            성사시키기
+          </Button>
+          <Button
+            variant="destructive"
+            onClick={() => {
+              handleStatusChange('REJECTED');
+            }}
+            className="w-[292px]"
+            disabled={isPending || !isWaiting}
+          >
+            거절하기
+          </Button>
+          <Button
+            variant="secondary"
+            onClick={() => {
+              toMyPage({ query: { tab: 'COFFEE_CHAT' } });
+            }}
+            className="w-[292px]"
+            disabled={isPending}
+          >
+            목록으로
+          </Button>
         </div>
       </div>
-    </div>
+      {responseMessage !== '' && (
+        <FormErrorResponse>{responseMessage}</FormErrorResponse>
+      )}
+    </>
   );
 };
---
+++ src/feature/coffeeChat/ui/detail/useCoffeeChatDetailHooks.ts
@@ -0,0 +1,77 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
+import type { CoffeeChatStatus } from '@/api/apis/localServer/schemas';
+import { useGuardContext } from '@/shared/context/hooks';
+import { ServiceContext } from '@/shared/context/ServiceContext';
+import { TokenContext } from '@/shared/context/TokenContext';
+import { useRouteNavigation } from '@/shared/route/useRouteNavigation';
+
+export const useGetCoffeeChatDetail = ({
+  coffeeChatId,
+}: {
+  coffeeChatId: string;
+}) => {
+  const { token } = useGuardContext(TokenContext);
+  const { coffeeChatService } = useGuardContext(ServiceContext);
+
+  const { data: coffeeChatDetailData } = useQuery({
+    queryKey: ['user', 'coffeeChat', coffeeChatId, token] as const,
+    queryFn: ({ queryKey: [, , , t] }) => {
+      if (t === null) {
+        throw new Error('토큰이 존재하지 않습니다.');
+      }
+      return coffeeChatService.getCoffeeChatDetail({
+        token: t,
+        coffeeChatId: coffeeChatId,
+      });
+    },
+    enabled: token !== null,
+  });
+
+  return { coffeeChatDetailData: coffeeChatDetailData };
+};
+
+export const useUpdateCoffeeChatStatus = ({
+  setResponseMessage,
+}: {
+  setResponseMessage(input: string): void;
+}) => {
+  const { coffeeChatService } = useGuardContext(ServiceContext);
+  const { token } = useGuardContext(TokenContext);
+  const { toMyPage } = useRouteNavigation();
+  const queryClient = useQueryClient();
+
+  const { mutate: updateCoffeeChatStatus, isPending } = useMutation({
+    mutationFn: ({
+      coffeeChatId,
+      body,
+    }: {
+      coffeeChatId: string;
+      body: { coffeeChatStatus: CoffeeChatStatus };
+    }) => {
+      if (token === null) {
+        throw new Error('토큰이 존재하지 않습니다.');
+      }
+      return coffeeChatService.updateCoffeeChatStatus({
+        token,
+        coffeeChatId,
+        body,
+      });
+    },
+    onSuccess: async (response) => {
+      if (response.type === 'success') {
+        await queryClient.invalidateQueries();
+        toMyPage({ query: { tab: 'COFFEE_CHAT' } });
+      } else {
+        setResponseMessage(response.code);
+      }
+    },
+    onError: () => {
+      setResponseMessage('취소에 실패했습니다. 잠시 후에 다시 실행해주세요.');
+    },
+  });
+  return {
+    updateCoffeeChatStatus,
+    isPending,
+  };
+};
