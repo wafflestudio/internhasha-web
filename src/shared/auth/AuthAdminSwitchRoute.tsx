e90d8522d47229ea261f653f53d5a30912cf8f73
Brian Kim <brian2562@gmail.com>
2025-03-26 13:54:58 +0900

    🐛 prettier fix

--- src/shared/auth/AuthAdminSwitchRoute.tsx
+++ src/shared/auth/AuthAdminSwitchRoute.tsx
@@ -1,8 +1,8 @@
 import type { ReactNode } from 'react';
 
 import { useGuardContext } from '@/shared/context/hooks';
-import { UserContext } from '@/shared/context/UserContext';
 import { TokenContext } from '@/shared/context/TokenContext';
+import { UserContext } from '@/shared/context/UserContext';
 
 export const AuthCompanySwitchRoute = ({
   companyPage,
--- src/shared/auth/ProtectedRoute.tsx
+++ src/shared/auth/ProtectedRoute.tsx
@@ -3,8 +3,8 @@ import { Outlet } from 'react-router';
 import { ReSignInModal } from '@/components/modal/ReSignInModal';
 import type { UserRole } from '@/entities/user';
 import { useGuardContext } from '@/shared/context/hooks';
-import { UserContext } from '@/shared/context/UserContext';
 import { TokenContext } from '@/shared/context/TokenContext';
+import { UserContext } from '@/shared/context/UserContext';
 import { PATH } from '@/shared/route/constants';
 import { RouteNavigator } from '@/shared/route/RouteNavigator';
 
