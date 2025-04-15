0cdc6159f0f091ace1d833b434c6ff163313c330
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-04-15 14:22:27 +0900

    🐛 회사 커피챗 뷰에 줄바꿈 적용

--- src/feature/coffeeChat/ui/detail/CompanyCoffeeChatDetailView.tsx
+++ src/feature/coffeeChat/ui/detail/CompanyCoffeeChatDetailView.tsx
@@ -4,6 +4,7 @@ import type { CoffeeChatCompany } from '@/api/apis/localServer/schemas';
 import { FormErrorResponse } from '@/components/response/formResponse';
 import { TagStatus } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
+import { TextareaPreview } from '@/components/ui/TextareaPreview';
 import { ApplicantInfoForCoffeeChat } from '@/feature/coffeeChat/ui/detail/ApplicantInfoForCoffeeChat';
 import { SkeletonCoffeeChatDetailView } from '@/feature/coffeeChat/ui/detail/SkeletonCoffeeChatDetailView';
 import {
@@ -60,9 +61,10 @@ export const CoffeeChatDetailView = ({
           <ApplicantInfoForCoffeeChat applicant={coffeeChatDetail.applicant} />
           <div className="flex flex-col gap-[20px]">
             <h3 className="text-22 font-bold">커피챗 신청 내용</h3>
-            <p className="font-regular text-gray-700">
-              {coffeeChatDetail.content}
-            </p>
+            <TextareaPreview
+              content={coffeeChatDetail.content}
+              className="font-regular text-gray-700"
+            />
           </div>
           <div className="flex gap-[10px] pt-[52px]">
             <Button
