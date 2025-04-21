4922b14e84b269d97747edf50cb8736f98f1c659
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-04-21 16:47:11 +0900

    💄 직군 선택 모달 UI 피그마와 동일하게 수정

--- src/feature/post/ui/form/fields/JobCategoryField.tsx
+++ src/feature/post/ui/form/fields/JobCategoryField.tsx
@@ -8,6 +8,7 @@ import { ICON_SRC } from '@/entities/asset';
 import type { Input } from '@/entities/input';
 import type { JobMajorCategory, JobMinorCategory } from '@/entities/post';
 import { JOB_CATEGORY_MAP, JOB_MAJOR_CATEGORIES } from '@/entities/post';
+import { cn } from '@/lib/utils';
 import { formatMajorJobToLabel, formatMinorJobToLabel } from '@/util/format';
 
 type JobCategoryFieldProps = {
@@ -49,9 +50,11 @@ export const JobCategoryField = ({
         variant="outline"
         className="w-full justify-between"
       >
-        {input.minor.value !== 'NONE'
-          ? formatMinorJobToLabel(input.minor.value)
-          : '직무를 선택해주세요.'}
+        <span className={input.minor.value === 'NONE' ? 'text-grey-500' : ''}>
+          {input.minor.value !== 'NONE'
+            ? formatMinorJobToLabel(input.minor.value)
+            : '직무를 선택해주세요.'}
+        </span>
         <img
           src={ICON_SRC.ARROW}
           className={`${showFilter === 'CATEGORY' ? 'rotate-0' : 'rotate-180'} transition-rotate duration-300 ease-in-out`}
@@ -59,13 +62,13 @@ export const JobCategoryField = ({
       </Button>
       <section className="relative">
         <div
-          className={`absolute left-0 top-0 z-50 flex w-[364px] gap-2 overflow-hidden rounded-lg bg-white shadow-lg transition-all duration-300 ${
+          className={`absolute left-0 top-0 z-50 flex w-[364px] gap-2 overflow-hidden rounded-lg bg-white p-3 pl-1 shadow-lg transition-all duration-300 ${
             showFilter === 'CATEGORY' && !isPending
               ? 'scale-100 opacity-100'
               : 'pointer-events-none opacity-0'
           }`}
         >
-          <div className="flex w-[162px] flex-col gap-2 border-r px-2">
+          <div className="flex w-[162px] flex-col gap-1 border-r px-2">
             {JOB_MAJOR_CATEGORIES.map((category) => {
               return (
                 <Button
@@ -76,14 +79,22 @@ export const JobCategoryField = ({
                     e.preventDefault();
                     input.major.onChange(category as JobMajorCategory);
                   }}
-                  className={`justify-start ${input.major.value === category ? 'bg-grey-50 font-bold' : ''}`}
+                  className={cn(
+                    'justify-between font-medium text-grey-900 hover:bg-grey-50',
+                    input.major.value === category
+                      ? 'bg-grey-50 font-bold'
+                      : '',
+                  )}
                 >
                   {formatMajorJobToLabel(category)}
+                  {input.major.value === category && (
+                    <img src={ICON_SRC.CHECK} />
+                  )}
                 </Button>
               );
             })}
           </div>
-          <div className="flex w-[202px] flex-col gap-2">
+          <div className="flex w-[202px] flex-col gap-1">
             {JOB_CATEGORY_MAP[input.major.value].map((subCategory) => (
               <Button
                 type="button"
@@ -95,9 +106,17 @@ export const JobCategoryField = ({
                   input.minor.onChange(subCategory);
                   onClick();
                 }}
-                className={`justify-start ${input.minor.value === subCategory ? 'bg-grey-50 font-bold' : ''}`}
+                className={cn(
+                  'justify-between font-medium text-grey-900 hover:bg-grey-50',
+                  input.minor.value === subCategory
+                    ? 'bg-grey-50 font-bold'
+                    : '',
+                )}
               >
                 {formatMinorJobToLabel(subCategory)}
+                {input.minor.value === subCategory && (
+                  <img src={ICON_SRC.CHECK} />
+                )}
               </Button>
             ))}
           </div>
