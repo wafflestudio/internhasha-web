850620da39c139c9f2f9931d1e72640b58cec106
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2025-05-04 00:21:15 +0900

    💄 구성 요소 전반의 일관성을 위해 텍스트 스타일과 모달 배경을 업데이트합니다.

--- src/feature/applicant/ui/modal/WithdrawModal.tsx
+++ src/feature/applicant/ui/modal/WithdrawModal.tsx
@@ -19,21 +19,19 @@ export const WithdrawModal = ({
       onOutSlideClick={handleCancel}
       className="text-grey-900"
     >
-      <div className="flex flex-col gap-[40px]">
-        <div className="flex flex-col gap-[14px] text-center">
-          <p className="text-18 font-bold">정말 탈퇴하시겠어요?</p>
-          <p className="text-16 font-medium">
-            탈퇴 시에는 가입했던 정보가 모두 삭제됩니다.
-          </p>
-        </div>
-        <div className="flex gap-2">
-          <Button variant="secondary" onClick={handleCancel} className="flex-1">
-            뒤로가기
-          </Button>
-          <Button variant="destructive" onClick={onAction} className="flex-1">
-            탈퇴하기
-          </Button>
-        </div>
+      <div className="flex flex-col gap-[14px] text-center">
+        <p className="text-18 font-bold">정말 탈퇴하시겠어요?</p>
+        <p className="text-16 font-medium">
+          탈퇴 시에는 가입했던 정보가 모두 삭제됩니다.
+        </p>
+      </div>
+      <div className="flex gap-2">
+        <Button variant="secondary" onClick={handleCancel} className="flex-1">
+          뒤로가기
+        </Button>
+        <Button variant="destructive" onClick={onAction} className="flex-1">
+          탈퇴하기
+        </Button>
       </div>
     </ModalFloatBackground>
   );
