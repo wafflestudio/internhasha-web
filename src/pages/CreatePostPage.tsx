13b2a0e8da09daf218aeebc046a97962270b3517
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-05-08 16:10:11 +0900

    💄 최대 너비 조정

--- src/pages/CreatePostPage.tsx
+++ src/pages/CreatePostPage.tsx
@@ -19,7 +19,7 @@ export const CreatePostPage = () => {
   return (
     <div className="min-h-screen">
       <GlobalNavigationBar />
-      <div className="mx-auto my-[30px] flex w-full flex-col justify-center gap-[50px] px-4 sm:w-screen-sm">
+      <div className="mx-auto my-[30px] flex w-full max-w-[280px] flex-col justify-center gap-[50px] px-4 sm:max-w-[700px]">
         {body !== undefined ? (
           <>
             <h2 className="text-30 font-bold text-grey-900">인턴 공고 수정</h2>
