dcc24cfce464b9e947ac009e801d614ed248d867
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2025-05-09 22:16:34 +0900

    ✨ 렌딩페이지 필터적용 누를때 모달 닫히기

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -7,6 +7,8 @@ const Popover = PopoverPrimitive.Root;
 
 const PopoverTrigger = PopoverPrimitive.Trigger;
 
+const PopoverClose = PopoverPrimitive.Close;
+
 // const PopoverAnchor = PopoverPrimitive.Anchor;
 
 const PopoverContent = React.forwardRef<
@@ -28,4 +30,4 @@ const PopoverContent = React.forwardRef<
 ));
 PopoverContent.displayName = PopoverPrimitive.Content.displayName;
 
-export { Popover, PopoverTrigger, PopoverContent };
+export { Popover, PopoverTrigger, PopoverContent, PopoverClose };
