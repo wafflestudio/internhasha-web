c284f65f4335cd3a7fa250a1a342481d7064721f
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-05-09 19:36:47 +0900

    💄 마크다운 에디터 변경 및 이미지 업로드 기능 삭제

--- src/components/field/MarkdownEditorField.tsx
+++ src/components/field/MarkdownEditorField.tsx
@@ -1,4 +1,7 @@
-import MDEditor from '@uiw/react-md-editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
+
+import { Editor, Viewer } from '@toast-ui/react-editor';
+import { useEffect, useRef } from 'react';
 
 import { LabelContainer } from '@/components/label/LabelContainer';
 import {
@@ -32,32 +35,62 @@ export const MarkdownEditorField = ({
   required,
   maxLength,
 }: MarkdownEditorFieldProps) => {
+  const editorRef = useRef<Editor>(null);
+
+  const handleChange = () => {
+    if (editorRef.current !== null) {
+      const editorInstance = editorRef.current.getInstance();
+      const markdown = editorInstance.getMarkdown();
+      input.onChange(markdown);
+    }
+  };
+
+  useEffect(() => {
+    if (editorRef.current !== null) {
+      const editorInstance = editorRef.current.getInstance();
+      if (input.value.length === 0) {
+        editorInstance.setMarkdown('');
+      }
+    }
+  }, [input]);
+
+  const toolbarItems = [
+    ['heading', 'bold', 'italic', 'strike'],
+    ['hr', 'quote'],
+    ['ul', 'ol', 'task', 'indent', 'outdent'],
+    ['table', 'link'],
+    ['code', 'codeblock'],
+  ];
+
+  if (isPending) {
+    <Viewer initialValue={input.value} />;
+  }
+
   return (
     <LabelContainer label={label} required={required}>
-      <div data-color-mode="light">
-        <MDEditor
-          textareaProps={{
-            disabled: isPending,
-            placeholder,
-          }}
-          value={input.value}
-          onChange={(value) => {
-            input.onChange(value !== undefined ? value : '');
-          }}
-        />
-        <div className="mt-1 flex flex-col gap-1">
-          <div className="flex w-full flex-col justify-between sm:flex-row">
-            <FormInfoResponse>{infoMessage}</FormInfoResponse>
-            <span
-              className={`text-sm ${input.value.length > maxLength ? 'text-red-300' : 'text-grey-500'}`}
-            >
-              {input.value.length}/{maxLength}
-            </span>
-          </div>
-          {isSubmit && isSubmitError && (
-            <FormErrorResponse>{errorMessage}</FormErrorResponse>
-          )}
+      <Editor
+        toolbarItems={toolbarItems}
+        initialValue={input.value.trim().length !== 0 ? input.value : ' '}
+        usageStatistics={false}
+        useCommandShortcut={true}
+        onChange={handleChange}
+        autofocus={false}
+        hideModeSwitch={true}
+        ref={editorRef}
+        placeholder={placeholder}
+      />
+      <div className="mt-1 flex flex-col gap-1">
+        <div className="flex w-full flex-col justify-between sm:flex-row">
+          <FormInfoResponse>{infoMessage}</FormInfoResponse>
+          <span
+            className={`text-sm ${input.value.length > maxLength ? 'text-red-300' : 'text-grey-500'}`}
+          >
+            {input.value.length}/{maxLength}
+          </span>
         </div>
+        {isSubmit && isSubmitError && (
+          <FormErrorResponse>{errorMessage}</FormErrorResponse>
+        )}
       </div>
     </LabelContainer>
   );
