83a2702bf19ac3cd0fab18976488267dd623e985
Yeonu Kim <88171936+Yeonu-Kim@users.noreply.github.com>
2025-05-09 20:51:02 +0900

    💄 마크다운 에디터 변경 및 이미지 업로드 기능 삭제 (#525)
    ### 📝 작업 내용
    
    - 더 UI가 예쁜 마크다운 에디터로 변경하였습니다. (Toast UI)
    - 이미지 삽입 버튼을 삭제하였습니다.

--- src/components/ui/markdown-preview.tsx
+++ src/components/ui/markdown-preview.tsx
@@ -1,12 +1,9 @@
-import MDEditor from '@uiw/react-md-editor';
+import { Viewer } from '@toast-ui/react-editor';
 
 export const MarkdownPreview = ({ content }: { content: string }) => {
   return (
-    <div
-      data-color-mode="light"
-      className="w-full p-4 font-regular text-grey-800"
-    >
-      <MDEditor.Markdown source={content} style={{ fontSize: '14px' }} />
+    <div className="w-full p-4 font-regular text-grey-800">
+      <Viewer initialValue={content} />
     </div>
   );
 };
